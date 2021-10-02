30d9d05c0ba87c176830a3c768e94b8fe29a4d9d
ALAM-97 <alexamara97@gmail.com>
2021-10-02 17:59:42 +0200

    Scaffolding

---
+++ src/components/Albums.vue
@@ -0,0 +1,15 @@
+<template>
+    <main>
+
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'Albums'
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '../style/common.scss'
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '../style/common.scss'
+
+</style>
