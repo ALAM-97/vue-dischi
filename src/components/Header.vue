94deef6a763af1616bb7c9a45cb5338524493fd2
ALAM-97 <alexamara97@gmail.com>
2021-10-04 16:14:43 +0200

    Select Added to Header Component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,13 @@
 <template>
     <header>
         <img src="../assets/spotify-logo.png" alt="">
+        <select name="" @change="$emit('filter', 'genre')">
+            <option :value="All" selected>Seleziona un genere...</option>
+            <option :value="Rock">Rock</option>
+            <option :value="Pop">Pop</option>
+            <option :value="Jazz">Jazz</option>
+            <option :value="Metal">Metal</option>
+        </select>
     </header>
 </template>
 
@@ -15,9 +22,18 @@ export default {
     
         header {
             background-color: $headerBg;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
             img {
                 width: 5%;
                 padding: 10px 0;
+            }
+            select {
+                width: 200px;
+                height: 30px;
+                border-radius: 5px;
+                margin-right: 20px;
             }
         }
 </style>
